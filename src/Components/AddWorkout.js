d112ffef6533dfc336a4c53c6438794c223113c4
Sheryl Hohman <SherylHohman@yahoo.com>
2021-10-19 19:47:51 -0700

    add nieve AddWorkout form fields, no styles

--- src/Components/AddWorkout.js
+++ src/Components/AddWorkout.js
@@ -5,6 +5,16 @@ function AddWorkout() {
     return (
         <div>
             <h1>Add Workout</h1>
+                <form>
+                    <label>Date</label>
+                    <input /><br /><br />
+                    <label>Hours</label>
+                    <input /><br /><br />
+                    <label>Minutes</label>
+                    <input /><br /><br />
+                    <label>(Cardio, Weights, Stretch)</label>
+                    <input /><br /><br />
+                </form>
         </div>
     );
 }
